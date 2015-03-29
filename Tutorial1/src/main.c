835dc008712e0b205198ab481d92e8c5f512938f
Andy Wilmot <awilmot@uwo.ca>
2015-03-28 23:04:21 -0400

    Yoi

--- Tutorial1/src/main.c
+++ Tutorial1/src/main.c
@@ -1,19 +1,15 @@
 #include <pebble.h>
   
-enum {
-  KEY_TEMPERATURE 0,
-  KEY_CONDITIONS 1
-};
-  
-static TextLayer *s_time_layer;
-static TextLayer *s_weather_layer;
-static GFont s_time_font;
-static GFont s_weather_font;
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_weather_layer;
  
 static GFont s_time_font;
+static GFont s_weather_font;
+ 
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
  
@@ -61,10 +57,7 @@ static void main_window_load(Window *window) {
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   
-  // Make sure the time is displayed from the start
-  update_time();
-  
-  // Create temperature layer
+  // Create temperature Layer
   s_weather_layer = text_layer_create(GRect(0, 130, 144, 25));
   text_layer_set_background_color(s_weather_layer, GColorClear);
   text_layer_set_text_color(s_weather_layer, GColorWhite);
@@ -76,6 +69,8 @@ static void main_window_load(Window *window) {
   text_layer_set_font(s_weather_layer, s_weather_font);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
   
+  // Make sure the time is displayed from the start
+  update_time();
 }
  
 static void main_window_unload(Window *window) {
@@ -98,20 +93,62 @@ static void main_window_unload(Window *window) {
  
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
-}
-
-static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   
+  // Get weather update every 30 minutes
+  if(tick_time->tm_min % 30 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+ 
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+ 
+    // Send the message!
+    app_message_outbox_send();
+  }
 }
-
+ 
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+  
+  // Read first item
+  Tuple *t = dict_read_first(iterator);
+ 
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+    case KEY_TEMPERATURE:
+      snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)t->value->int32);
+      break;
+    case KEY_CONDITIONS:
+      snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+      break;
+    default:
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+      break;
+    }
+ 
+    // Look for next item
+    t = dict_read_next(iterator);
+  }
+  
+  // Assemble full string and display
+  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+  text_layer_set_text(s_weather_layer, weather_layer_buffer);
+}
+ 
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
   APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
 }
-
+ 
 static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
   APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
 }
-
+ 
 static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
@@ -137,7 +174,7 @@ static void init() {
   app_message_register_inbox_dropped(inbox_dropped_callback);
   app_message_register_outbox_failed(outbox_failed_callback);
   app_message_register_outbox_sent(outbox_sent_callback);
-
+  
   // Open AppMessage
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
